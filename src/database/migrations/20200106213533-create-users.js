d2316f80d70870f4e26d26fc71b650d2f36971c4
alsdede <andreluizsilveira.88@gmail.com>
2020-01-07 17:04:52 -0300

    config connection sequelize db/model

--- src/database/migrations/20200106213533-create-users.js
+++ src/database/migrations/20200106213533-create-users.js
@@ -8,13 +8,30 @@ module.exports = {
                 primaryKey: true,
             },
             name: {
-                type: Sequelize.INTEGER,
+                type: Sequelize.STRING,
                 allowNull: false,
             },
             email: {
-                type: Sequelize.INTEGER,
+                type: Sequelize.STRING,
                 allowNull: false,
                 unique: true,
+            },
+            password_hash: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            provider: {
+                type: Sequelize.BOOLEAN,
+                defaultValue: false,
+                allowNull: false,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
             },
         });
     },
